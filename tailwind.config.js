b678c8b63f39ee0d8150a40df549eabe4e48ebf9
kauan f <kauanfelix10.a@gmail.com>
2024-03-29 02:25:53 -0300

    instalacao do tailwid

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'orange': '#e2865',
+      'green': '#006971',
+      'dark-green': '#004140',
+      'offwhite': 'rgba(255,255,255,0.75)',
+      'white': '#fff',
+      'offwhitebg': ''#f2f2f2'
+    }
+  },
+  plugins: [],
+}
