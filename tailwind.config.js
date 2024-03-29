1faca6f1c361698c84249b6715e10f8f3cc4cbc1
kauan f <kauanfelix10.a@gmail.com>
2024-03-29 10:38:28 -0300

    terceiro commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,13 @@ export default {
       'dark-green': '#004140',
       'offwhite': 'rgba(255,255,255,0.75)',
       'white': '#fff',
-      'offwhitebg': ''#f2f2f2'
+      'offwhitebg': '#f2f2f2'
+    },
+    fontFamily: {
+      'shoulders': '"Big Shoulders Display"',
+      'lexend': '"Lexend Deca"'
     }
   },
+  
   plugins: [],
 }
